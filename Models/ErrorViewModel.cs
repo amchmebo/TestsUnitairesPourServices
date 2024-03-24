383203303136a84364a878072188e81ca90c1b1a
Mathieu Briau <mathieu.briau@gmail.com>
2024-03-24 12:07:05 -0400

    Retrait (utilisait un projet directement dans la solution....)

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TestsUnitairesPourServices.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
